9322bf1619692d5000dab06e731f37a177b6802e
drfricke <46977167+drfricke@users.noreply.github.com>
2019-01-30 16:07:27 -0500

    Add files via upload

---
+++ Bubble_level.py
@@ -0,0 +1,27 @@
+import math
+import microbit
+from microbit import *
+
+# microbit SOP - Flash, REPL, Reset (button near jack on the mircobit.
+# Remember to be in microbit mode
+
+while True:
+    microbit.sleep(250)
+    print(microbit.accelerometer.get_values())
+    y = microbit.accelerometer.get_y()
+    #Want these values for atan2()
+    x = microbit.accelerometer.get_x()
+    orientation = ((math.atan2(y, x))/(math.pi))*180
+    print(orientation)
+    #want this relationship to find angles
+    while True:
+        if orientation < -90:
+            display.show(Image.ARROW_NW)
+        elif orientation > -90 and orientation < 0:
+            display.show(Image.ARROW_NE)
+        elif orientation > 90:
+            display.show(Image.ARROW_SW)
+        elif orientation < 90 and orientation > 0:
+            display.show(Image.ARROW_SE)
+        else:
+            display.show(Image("00000:00000:00900:00000:00000"))
